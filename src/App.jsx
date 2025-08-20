a72873a3b5f54688376a9e0464a0b77d634b9a14
anusha-anil3773 <anushaanil3773@gmail.com>
2025-08-21 00:37:50 +0530

    feat: add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   )
